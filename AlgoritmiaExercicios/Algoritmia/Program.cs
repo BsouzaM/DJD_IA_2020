973e4ba95ed89e6270662f7406bb89c10160d653
BsouzaM <bernardo_429@hotmail.com>
2020-10-01 20:14:31 +0100

    Program.cs atualizado

--- AlgoritmiaExercicios/Algoritmia/Program.cs
+++ AlgoritmiaExercicios/Algoritmia/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Algoritmia
 {
@@ -8,7 +9,20 @@ namespace Algoritmia
         {
             ExerciciosAlgoritmia eA = new ExerciciosAlgoritmia();
             Console.WriteLine("Hello World!");
+
+            Thread.Sleep(2000);
             eA.LetTheGamesBegin();
+            Thread.Sleep(2000);
+            eA.Noob();
+            Thread.Sleep(2000);
+            eA.EasyPeasy();
+            Thread.Sleep(2000);
+            eA.ForAzeroth();
+            Thread.Sleep(2000);
+            eA.Frostmourne();
+            Thread.Sleep(2000);
+            eA.GeeGee();
+            Thread.Sleep(5000);
         }
     }
 }
